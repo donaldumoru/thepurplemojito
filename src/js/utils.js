7a89238b5351041f2667b077c98ef8a1ca052e80
donaldumoru <donaldumoru101@gmail.com>
2026-07-09 22:18:42 +0200

    initial commit v2-react

--- src/js/utils.js
+++
@@ -1,6 +0,0 @@
-export const getRandomNumber = (min, max) => Math.random() * (max - min) + min;
-
-export const toggleVisibility = function (view, classNames) {
-  view.classList.toggle(classNames.active);
-  view.classList.toggle(classNames.inactive);
-};
