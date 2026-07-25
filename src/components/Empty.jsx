91fcd150c0f60400a00d02e1a64efd56e2687f6a
donaldumoru <donaldumoru101@gmail.com>
2026-07-25 14:41:58 +0200

    layout refactor - empty container

--- src/components/Empty.jsx
+++ src/components/Empty.jsx
@@ -2,7 +2,7 @@ import stamp from '../assets/icons/stamp.webp';
 
 function Empty({ children }) {
   return (
-    <section className="my-16 flex w-full flex-col items-center">
+    <section className="flex w-full flex-col items-center sm:my-16">
       <div className="max-w-1/2 sm:max-w-1/6">
         <img className="w-full" src={stamp} alt="image of a stamp" />
       </div>
